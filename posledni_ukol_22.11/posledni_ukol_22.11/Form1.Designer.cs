62496619acc5f651c1c2d37c79bed626f50c3080
Filip Iran <72192710+F1lgas@users.noreply.github.com>
2021-12-07 14:43:34 +0100

    Add files via upload

---
+++ posledni_ukol_22.11/posledni_ukol_22.11/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace posledni_ukol_22._11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.vstup = new System.Windows.Forms.TextBox();
+            this.hledat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // vstup
+            // 
+            this.vstup.Location = new System.Drawing.Point(13, 13);
+            this.vstup.Multiline = true;
+            this.vstup.Name = "vstup";
+            this.vstup.Size = new System.Drawing.Size(100, 119);
+            this.vstup.TabIndex = 0;
+            // 
+            // hledat
+            // 
+            this.hledat.Location = new System.Drawing.Point(120, 13);
+            this.hledat.Name = "hledat";
+            this.hledat.Size = new System.Drawing.Size(75, 23);
+            this.hledat.TabIndex = 1;
+            this.hledat.Text = "Hledat";
+            this.hledat.UseVisualStyleBackColor = true;
+            this.hledat.Click += new System.EventHandler(this.hledat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 143);
+            this.Controls.Add(this.hledat);
+            this.Controls.Add(this.vstup);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Poslední úkol";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox vstup;
+        private System.Windows.Forms.Button hledat;
+    }
+}
+
---
+++ posledni_ukol_22.11/posledni_ukol_22.11/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace posledni_ukol_22._11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hledat_Click(object sender, EventArgs e)
+        {
+            int min = 0, i = 0;
+            bool pravda = false;
+
+            while (i <= vstup.Lines.Length && pravda == false)
+            {
+                try
+                {
+                    if (Convert.ToInt32(vstup.Lines[i]) % 2 == 0)
+                    {
+                        min = Convert.ToInt32(vstup.Lines[i]);
+                        pravda = true;
+
+                        if (Convert.ToInt32(vstup.Lines[i]) < min && pravda == true)
+                        {
+                            min = Convert.ToInt32(vstup.Lines[i]);
+                        }
+                    }
+                }
+                catch
+                {
+
+                }
+                i++;
+            }
+
+            i = 0;
+
+            try
+            {
+                while (i < vstup.Lines.Length)
+                {
+                    if (Convert.ToInt32(vstup.Lines[i]) < min && Convert.ToInt32(vstup.Lines[i]) % 2 == 0)
+                    {
+                        min = Convert.ToInt32(vstup.Lines[i]);
+                    }
+                    i++;
+                }
+
+                if (pravda == false)
+                {
+                    MessageBox.Show("V textBoxu nejsou sudá čísla", "Výsledek");
+                }
+                else
+                {
+                    MessageBox.Show("Nejmenší sudé číslo v textBoxu je " + min, "Výsledek");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Můžeš zadat pouze čísla!", "Error");
+            }
+        }
+    }
+}
